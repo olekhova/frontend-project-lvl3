df4e5c334405f0d5333a995f1136a4a8243ca887
Lyubov Olekhova <olekhova@gmail.com>
2021-06-09 00:56:23 +0300

    webpack bootstrap trying....

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,14 @@
+const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
   mode: process.env.NODE_ENV || 'development',
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
   module: {
     rules: [
       {
@@ -10,12 +17,25 @@ module.exports = {
         use: {
           loader: 'babel-loader',
         },
-      },
+      }, {
+        test: /\.(scss)$/,
+        use: [{
+          loader: 'style-loader', // inject CSS to page
+        }, {
+          loader: 'css-loader', // translates CSS into CommonJS modules
+        }, {
+          loader: 'postcss-loader', // Run postcss actions          
+        }, {
+          loader: 'sass-loader' // compiles Sass to CSS
+        }]
+      }
     ],
   },
   plugins: [
     new HtmlWebpackPlugin({
       template: 'template.html',
     }),
+    new CleanWebpackPlugin()
   ],
+  
 };
